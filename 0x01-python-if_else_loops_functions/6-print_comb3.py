72f3c133cac5c70cf14066cefc7dce003b8ea372
Nadee381 <Nadenesamuels98@gmail.com>
2023-06-07 12:21:53 -0700

    if/else statements

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 for n in range(10):
     for d in range(n + 1, 10):
-        print("{:d}{:d}, ".format(n, d), end="")
-print()
+        print("{:d}".format(n), end="")
+        print("{:d}".format(d), end="")
+        if n != 8 or d != 9:
+            print(", ", end="")i
