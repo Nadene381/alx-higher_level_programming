3675ef1d317885b9183ba9ec4abf26087581ad43
Nadee381 <Nadenesamuels98@gmail.com>
2023-06-07 10:00:35 -0700

    if/else statements

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+number_last_val = abs(number) % 10
+output_message = f"The string Last digit of {number} is {number_last_val}"
+if number_last_val == 0:
+    output_message += " and is 0"
+elif number_last_val > 5:
+    output_message += " and is greater than 5"
+else:
+    output_message += f" and is less than 6 and not 0"
+print(output_message)
