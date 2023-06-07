7309b4d0553eadaba2d0e8e4d53945256ae6ad02
Nadee381 <Nadenesamuels98@gmail.com>
2023-06-07 11:45:42 -0700

    if/else statements

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for n in str:
+        if 'a' <= c <= 'z':
+            uppercase_char = chr(ord(n) & ~32)
+        else:
+            uppercase_char = n
+        print("{}".format(uppercase_char), end='')
+    print()
