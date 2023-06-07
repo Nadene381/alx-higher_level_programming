73cae8768eceb3b5538b1a4ecca4f64147326799
Nadee381 <Nadenesamuels98@gmail.com>
2023-06-07 10:29:53 -0700

    if/else statements

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print("".join("{}".format(chr(n)) for n in range(ord('a'), ord('z') + 1)), end="")
+print("".join("{}".format(chr(n))
+               for n in range(ord('a'), ord('z') + 1)),
+      end="")
