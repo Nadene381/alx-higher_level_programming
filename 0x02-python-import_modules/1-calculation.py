8d197f3412e551c96fdeda8644142418b93a9d31
Nadee381 <Nadenesamuels98@gmail.com>
2023-06-08 13:57:47 -0700

    import & modules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,9 +2,9 @@
 from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
+answer_of_add = add(a, b)
 answer_of_sub = sub(a, b)
 answer_of_mul = mul(a, b)
-answer_of_add = add(a, b)
 answer_of_div = div(a, b)
 print("{} + {} = {}".format(a, b, answer_of_add))
 print("{} - {} = {}".format(a, b, answer_of_sub))
