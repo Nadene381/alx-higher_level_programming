f2170a83934f00965552408b33a92e519734b309
Nadee381 <Nadenesamuels98@gmail.com>
2023-06-08 14:14:42 -0700

    import & modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+number_argum = len(sys.argv) - 1
+if number_argum == 0:
+    print(F"{number_argum} arguments.")
+elif number_argum == 1:
+    print(F"{number_argum} argument:")
+    print(f"1: {sys.argv[1]}")
+else:
+    print(F"{number_argum} arguments:")
+    n = 1
+    while n <= number_argum:
+        print("{}: {}".format(n, sys.argv[n]))
+        n += 1
