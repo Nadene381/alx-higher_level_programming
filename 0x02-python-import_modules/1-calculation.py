a20e422bb09c2be0b66d617436b245ed41db3593
Nadee381 <Nadenesamuels98@gmail.com>
2023-06-08 13:50:28 -0700

    import & modules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
-if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
 answer_of_sub = sub(a, b)
 answer_of_mul = mul(a, b)
 answer_of_add = add(a, b)
