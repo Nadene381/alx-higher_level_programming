f0a2c4684492424d1d9d99d589d9fc97d25c6da9
Nadee381 <Nadenesamuels98@gmail.com>
2023-06-08 13:34:32 -0700

    import & modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 a = 1
 b = 2
-if __main_variable__ == "__main__":
+if __name__ == "__main__":
     from add_0 import add
 get_result = add(a, b)
 print("{a} + {b} = {result}".format(a=a, b=b, result=get_result))
