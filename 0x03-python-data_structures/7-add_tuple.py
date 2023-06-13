f3420b79943b0d9f2d7d1995e153f9a10b66b1ba
Nadee381 <Nadenesamuels98@gmail.com>
2023-06-13 12:39:42 -0700

    Data Structures: Lists, Tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+tupleA = (1, 2)
+tupleB = (3, 4)
+result = add_tuple(tupleA, tupleB)
+print(result)
+
+tupleC = (5,)
+tupleD = (6, 7, 8)
+result = add_tuple(tupleC, tupleD)
+print(result)
+
+tupleE = ()
+tupleF = ()
+result = add_tuple(tupleE, tupleF)
+print(result)
