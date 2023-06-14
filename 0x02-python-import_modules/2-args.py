5bacb80f6a31d68a960f2c0911f1c52af7d3c5d1
Nadee381 <Nadenesamuels98@gmail.com>
2023-06-14 12:00:57 -0700

    import & modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,15 +2,15 @@
 import sys
 number_argum = len(sys.argv) - 1
 if number_argum == 0:
-    print(F"{number_argum} arguments.")
+    print(f"{number_argum} arguments.")
 elif number_argum == 1:
-    print(F"{number_argum} argument:")
+    print(f"{number_argum} argument:")
     print(f"1: {sys.argv[1]}")
 else:
-    print(F"{number_argum} arguments:")
+    print(f"{number_argum} arguments:")
     n = 1
     while n <= number_argum:
-        print("{}: {}".format(n, sys.argv[n]))
+         print(f"{n}: {sys.argv[n]}")
         n += 1
     if __name__ == "__main__":
     pass
