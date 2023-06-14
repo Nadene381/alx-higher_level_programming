add02dc89fd14eb7633ad514cf51fee3c8bbb78a
Nadee381 <Nadenesamuels98@gmail.com>
2023-06-14 12:57:25 -0700

    More Data Structures: Set, Dictionary

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    keys_to_remove = []
+    for key, val in a_dictionary.items():
+        if val == value:
+            keys_to_remove.append(key)
+    for key in keys_to_remove:
+        del a_dictionary[key]
+    return a_dictionary
