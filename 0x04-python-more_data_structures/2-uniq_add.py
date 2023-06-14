351f769a62b519199422530c753d55eb70d0dbb6
Nadee381 <Nadenesamuels98@gmail.com>
2023-06-14 11:48:24 -0700

    More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def uniq_add(my_list):
-    unique_int = set(my_list) 
-    unique_int_sum = sum(unique_int)  
+    unique_int = set(my_list)
+    unique_int_sum = sum(unique_int)
     return unique_int_sum
