30026c8f476ba076bdc7572ccb0050a299a916d8
Nadee381 <Nadenesamuels98@gmail.com>
2023-06-14 11:24:10 -0700

    More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key):
-    if key in updated_dictionary:
-        del updated_dictionary[key]
-    return updated_dictionary
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
