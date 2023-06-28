5798ea79d83906cbdd0f17d540f89a1b451a49a6
Nadee381 <Nadenesamuels98@gmail.com>
2023-06-28 11:33:35 -0700

    Exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for i in my_list[:x]:
             print("{}".format(i), end=' ')
             index += 1
-        print()
+        print("")
         return index
     except Error:
         print("Invalid element")
