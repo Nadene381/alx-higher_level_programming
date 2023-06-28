438ed3bf30b953c311d99941d2d492d2650ddfe9
Nadee381 <Nadenesamuels98@gmail.com>
2023-06-28 11:08:27 -0700

    Exceptions

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if int(value) == value:
-            print("{:d}".format(value))
-            print()
-            return True
-        else:
-            return False
+        print("{:d}".format(value))
+        return True
     except (ValueError, TypeError):
         return False
