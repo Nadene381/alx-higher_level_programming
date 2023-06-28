6eca672719ca92f2aa5bb8eb0ec346496f2c2dfc
Nadee381 <Nadenesamuels98@gmail.com>
2023-06-28 13:14:27 -0700

    python-classes

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
 '''
-This module consist of a class that defines a square.
+This module contains a class that defines a square.
 '''
-
 
 class Square:
     """
@@ -19,6 +19,7 @@ class Square:
             ValueError: If size is less than 0.
         '''
         self.size = size
+
     @property
     def size(self):
         '''
@@ -27,7 +28,8 @@ class Square:
             int: The size of the square.
         '''
         return self.__size
-     @size.setter
+
+    @size.setter
     def size(self, value):
         '''
         Sets the size of the square.
@@ -37,14 +39,15 @@ class Square:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
         '''
-         if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
+
     def area(self):
         '''
-        Calculate and returns the area of a square.
+        Calculates and returns the area of a square.
         Returns:
             int: The area of the square.
         '''
