a49202e0872fa8438bd54fbfe78a5188729249be
Nadee381 <Nadenesamuels98@gmail.com>
2023-07-31 07:06:51 -0700

    Python - Input/Output

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A function that returns the JSON representation of an object (string)"""
+import json
+
+
+def to_json_string(my_obj):
+    """ Function that returns JSON representation(string)"""
+  
+    return json.dumps(my_obj)
