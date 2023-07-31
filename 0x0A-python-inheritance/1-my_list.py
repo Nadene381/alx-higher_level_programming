a25634589631dfa1fdfa5b2b5c6593b62823e8ba
Nadee381 <Nadenesamuels98@gmail.com>
2023-07-31 10:12:31 -0700

    Inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,9 +5,7 @@
 
 
 class MyList(list):
-    '''
-        This prints a list
-    '''
+    '''This prints a list'''
     def print_sorted(self):
-         """Function that Prints the list but sorted"""
-         print(sorted(self))
+        """Function that Prints the list but sorted"""
+        print(sorted(self))
