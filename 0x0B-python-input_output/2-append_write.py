34092e0f523fc9af6df807d81815ae30c12eb7b1
Nadene381 <69760379+Nadene381@users.noreply.github.com>
2023-08-21 11:08:29 +0200

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/2-append_write.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""A function that appends a string at the end of a text file"""
-
-
-def append_write(filename="", text=""):
-    """ Function that append to a file"""
-
-    with open(filename, mode='a', encoding='utf-8') as f:
-        return f.write(text)
