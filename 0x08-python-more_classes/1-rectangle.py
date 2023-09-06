6b30880a922ec8ed7d16aa59ef17aeb605939b92
Nadene381 <Nadenesamuels98@gmail.com>
2023-09-06 07:40:37 +0000

    Python - more classes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+"""
+   This module defines a rectangle class
+"""
+
+
+class Rectangle:
+    """
+    This class represents a Rectangle.
+
+    """
+    pass
 
 """
 This module represents a rectangle class
