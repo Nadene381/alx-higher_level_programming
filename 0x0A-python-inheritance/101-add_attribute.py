722083e420b50fee5b59394763fb0b03db27449b
Nadene381 <Nadenesamuels98@gmail.com>
2023-10-02 15:30:02 +0000

    Python Inheritance

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+    function to set an atributte
+'''
+
+
+def add_attribute(obj, name, value):
+    '''
+        add an antributte or raise an error when object has not attributes
+    '''
+    if not hasattr(obj, '__dict__'):
+        raise TypeError("can't add new attribute")
+    else:
+        setattr(obj, name, value)
